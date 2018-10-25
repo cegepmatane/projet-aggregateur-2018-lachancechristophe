33620f478cd4cc85062310e3a3109d5984189af8
Christophe Lachance <lachance.christophe@cgmatane.qc.ca>
2018-10-25 08:53:48 -0400

    Premier Commit
    Création de la solution et début de l'écriture du DAO PokéApi.

---
+++ PokeapiDAO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2_ProjetAgregateur
+{
+    class PokeapiDAO
+    {
+        string baseURL = "https://pokeapi.co/api/v2/";
+        string requestURL = "pokemon/";
+
+        void GetPokemon()
+        {
+            string completeURL = baseURL + requestURL;
+        }
+    }
+}
